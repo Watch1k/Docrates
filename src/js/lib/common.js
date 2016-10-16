61c3c471ddf49d37937cf82b1745a43463978ae0
watch1k <host.sf@gmail.com>
2016-10-16 22:52:18 +0300

    update

--- src/js/lib/common.js
+++ src/js/lib/common.js
@@ -4,23 +4,6 @@ $(document).ready(function(){
 	//for IE9
 	svg4everybody();
 
-	// detect phone/tablet
-	var md = new MobileDetect(window.navigator.userAgent);
-	console.log(md.phone());
-	console.log(md.tablet());
-
-	// check orientation device
-	function isPortrait() {
-		return window.innerHeight > window.innerWidth;
-	};
-
-	// Listen for orientation changes
-	window.addEventListener("orientationchange", function() {
-		if (isPortrait()) {
-			// code here
-		}
-	}, false);
-
 	// Clear placeholder
 	(function() {
 		var el = $('input, textarea');
@@ -34,5 +17,14 @@ $(document).ready(function(){
 	}());
 
 	// new WOW().init();
+
+	(function () {
+		var btn = $('.js-more'),
+			content = btn.siblings('.js-text');
+
+		btn.on('click', function () {
+			content.slideToggle();
+		});
+	})();
 	
 });
